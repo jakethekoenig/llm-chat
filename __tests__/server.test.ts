acbbae8fab373cb1112468f0bd1f7ad5de02b95a
Jake Koenig <jakethekoenig@gmail.com>
2024-08-26 19:38:02 -0700

    Split app out of index

--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -1,48 +1,6 @@
 import request from 'supertest';
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const app = express();
-const SECRET_KEY = process.env.SECRET_KEY || 'fallback-secret-key';
-
-app.use(bodyParser.json());
-app.use(cors());
-
-// Mock user data
-const users = [
-  { id: 1, username: 'user1', password: 'password1' },
-  { id: 2, username: 'user2', password: 'password2' }
-];
-
-// Sign-in route
-app.post('/signin', (req: express.Request, res: express.Response) => {
-  const { username, password } = req.body;
-  const user = users.find(u => u.username === username && u.password === password);
-  if (user) {
-    const token = jwt.sign({ id: user.id }, SECRET_KEY as jwt.Secret, { expiresIn: '1h' });
-    res.json({ token });
-  } else {
-    res.status(401).send('Invalid credentials');
-  }
-});
-
-// Middleware to verify token
-const authenticateToken = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (!token) return res.sendStatus(401);
-
-  jwt.verify(token, SECRET_KEY as jwt.Secret, (err: jwt.VerifyErrors | null, decoded: object | undefined) => {
-    if (err) return res.sendStatus(403);
-    (req as any).user = decoded;
-    next();
-  });
-};
+import app from '../server/app'
 
 // Streaming endpoint
 app.get('/get_completion', authenticateToken, (req: express.Request, res: express.Response) => {
